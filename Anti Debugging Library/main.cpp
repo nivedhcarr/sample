61991749e8214618f28fa0ee0e4dd6145a0a22ff
nivedhcarr <nivedhkeng@gmail.com>
2026-04-10 16:17:48 +0100

    Update main.cpp

--- Anti Debugging Library/main.cpp
+++ Anti Debugging Library/main.cpp
@@ -1,1 +1,20 @@
+#include "Antidebug.h"
+#include<iostream>
+#include <Windows.h>
+#include <intrin.h>  // Required for __readgsqword
+
+int main(){
+	AntiDebugger::HideThread(); // Phase 5: Make the main thread invisible to debuggers
+    // Check all phases at once
+    if (AntiDebugger::IsBeingDebugged()) {
+        // Exit silently to confuse the analyst
+        return 0;
+    }
+    std::cout << "Secure execution started..." << std::endl;
+    // ... your sensitive code here ...(We could not detect any debuggers , so you can write your sensitive code now)
+    
+
+    return 0;
+
+}
 
